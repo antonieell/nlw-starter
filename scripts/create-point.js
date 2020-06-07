d9583894f9ccd5729bc1592ed26d20eae2bcf186
Antoniel Magalhães <antoniel2210@gmail.com>
2020-06-07 08:35:59 -0300

    #3 - Até 2 anos em 2 meses

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -18,6 +18,8 @@ function getCity(event) {
 
     const indexOfSelectedState = event.target.selectedIndex;
     stateInput.value = event.target.options[indexOfSelectedState].text;
+    citySelect.innerHTML = `Selecione a cidade`;
+    citySelect.disabled = true;
 
     const id = event.target.value;
     fetch(
@@ -26,10 +28,47 @@ function getCity(event) {
         .then((res) => res.json())
         .then((city) => {
             city.forEach((value, index) => {
-                citySelect.innerHTML += `<option value="${value.id}">${value.nome}</option>`;
+                citySelect.innerHTML += `<option value="${value.nome}">${value.nome}</option>`;
             });
         });
     citySelect.disabled = false;
 }
-
 populateUFs();
+
+//Ítens de coleta
+
+const itemsToColect = document.querySelectorAll(".items-grid li");
+
+itemsToColect.forEach((value) =>
+    value.addEventListener("click", handleSelectedItem),
+);
+
+const collectedItems = document.querySelector("input[name=items]");
+let selectedItems = [];
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+    //Add or remove a class with JavaScript
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    //Verificar se existem itens selecionados ?
+    const alreadySelected = selectedItems.findIndex((item) => {
+        const itemFound = item == itemId;
+        return itemFound;
+    });
+    //Se já está selecionado tirar da seleção
+    console.log(alreadySelected);
+    if (alreadySelected >= 0) {
+        const filtredItems = selectedItems.filter((item) => {
+            return item != itemId;
+        });
+        selectedItems = filtredItems;
+    } else {
+        //Adiciona à seleção
+        selectedItems.push(itemId);
+    }
+    console.log(selectedItems);
+    collectedItems.value = selectedItems;
+    //Adicioanr ao campo escondido
+}
