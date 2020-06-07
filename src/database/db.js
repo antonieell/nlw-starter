f3ec724b405a59c27cfb61841be5a37862af7474
Antoniel Magalhães <antoniel2210@gmail.com>
2020-06-07 13:30:45 -0300

    Nlw - End

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,14 @@
 // importar a dependencia do sqlite3
-const sqlite3 = require("sqlite3").verbose()
+const sqlite3 = require("sqlite3").verbose();
 
 // criar o objeto que irá fazer operações no banco de dados
-const db = new sqlite3.Database("./src/database/database.db")
+const db = new sqlite3.Database("./src/database/database.db");
 
-module.exports = db
+module.exports = db;
 // utilizar o objeto de banco de dados, para nossas operações
 db.serialize(() => {
-
     // com commandos SQL eu vou:
-
-    // // 1 Criar uma tabela 
+    // // 1 Criar uma tabela
     // db.run(`
     //     CREATE TABLE IF NOT EXISTS places (
     //         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,7 +21,6 @@ db.serialize(() => {
     //         items TEXT
     //     );
     // `)
-
     // // 2 Inserir dados na tabela
     // const query = `
     //     INSERT INTO places (
@@ -36,7 +33,6 @@ db.serialize(() => {
     //         items
     //     ) VALUES (?,?,?,?,?,?,?);
     // `
-
     // const values = [
     //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     //     "Papersider",
@@ -46,36 +42,27 @@ db.serialize(() => {
     //     "Rio do Sul",
     //     "Resíduos Eletrônicos, Lâmpadas"
     // ]
-
     // function afterInsertData(err) {
     //     if(err) {
     //         return console.log(err)
     //     }
-
     //     console.log("Cadastrado com sucesso")
     //     console.log(this)
     // }
-
     // db.run(query, values, afterInsertData)
-
-
     // 3 Consultar os dados da tabela
     // db.all(`SELECT name FROM places`, function(err, rows) {
     //     if(err) {
     //         return console.log(err)
     //     }
-
     //     console.log("Aqui estão seus registros: ")
     //     console.log(rows)
     // })
-
     // 4 Deletar um dado da tabela
     // db.run(`DELETE FROM places WHERE id = ?`, [3], function(err) {
     //     if(err) {
     //         return console.log(err)
     //     }
-
     //     console.log("Registro deletado com sucesso!")
     // })
-
-})
+});
